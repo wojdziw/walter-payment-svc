80197065c0c408c1bce4d6be97e39c3cdd5b94c4
wojdziw <wojciech.dziwulski@gmail.com>
2017-07-30 20:23:34 +0200

    lol

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'hello',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
