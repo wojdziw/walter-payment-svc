08f1a38d1a2d1841f993208d8d9c77139a21ecda
wojdziw <wojciech.dziwulski@gmail.com>
2017-07-30 21:07:17 +0200

    haha

--- hello/models.py
+++ hello/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from rest_framework.parsers import BaseParser
 
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+
+class PlainTextParser(BaseParser):
+    """
+    Plain text parser.
+    """
+    media_type = 'text/plain'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        """
+        Simply return a string representing the body of the request.
+        """
+        return stream.read()
--- hello/views.py
+++ hello/views.py
@@ -1,23 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.parsers import JSONParser
 import requests
 import os
 
-from .models import Greeting
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.decorators import parser_classes
+
+
+from django.http import HttpResponse
+
+from rest_framework.response import Response
+
+from .models import Greeting, PlainTextParser
 
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',3))
     return HttpResponse('Hello! ' * times)
 
+
 def waiterapp(request):
     times = int(os.environ.get('TIMES',3))
     return HttpResponse('Hello! ' * times)
     # return HttpResponse('<meta http-equiv="refresh" content="0; URL=\'waiterapp://\'" />')
 
+@api_view(['POST'])
+@parser_classes((PlainTextParser,))
 def transactionStatus(request):
-    times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Transaction status!')
+    if request.method == 'POST':
+        print(request.data);
+    
+    return HttpResponse(status=200)
+        
 
 
 def db(request):
