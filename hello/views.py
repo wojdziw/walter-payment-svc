c077617b6d6aa0a218e760cf75d9d826e4168880
wojdziw <wojciech.dziwulski@gmail.com>
2017-07-30 18:51:07 +0200

    lol

--- hello/views.py
+++ hello/views.py
@@ -12,7 +12,7 @@ def index(request):
 
 def waiterapp(request):
     times = int(os.environ.get('TIMES',3))
-    return HttpResponse('<meta http-equiv="refresh" content="0; URL='http://developers.payu.com/pl/restapi.html'" />')
+    return HttpResponse('<meta http-equiv="refresh" content="0; URL=\'http://developers.payu.com/pl/restapi.html\'" />')
 
 
 def db(request):
