b729016c11963db78b2d6a37c9a47a7af2ffc35a
wojdziw <wojciech.dziwulski@gmail.com>
2017-07-31 17:26:31 +0200

    some changes

--- hello/views.py
+++ hello/views.py
@@ -77,7 +77,7 @@ def getPaymentUri(request):
 
     request = requests.post("https://secure.snd.payu.com/api/v2_1/orders/", json=content, headers=headers)
     
-    print ("!!!!!!!!!!!!!!!!"+request.json())
+    print ("!!!!!!!!!!!!!!!!"+request.text)
 
     return HttpResponse(request.text)
 
