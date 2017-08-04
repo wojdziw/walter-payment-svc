eb2048e237d0cb8f50ce729127ccf517839995a5
wojdziw <wojciech.dziwulski@gmail.com>
2017-08-04 12:42:46 +0200

    some changes

--- hello/views.py
+++ hello/views.py
@@ -52,7 +52,7 @@ def getToken():
 def getPaymentUri(request):
     totalAmount = request.data['totalAmount']
     token = getToken()
-    print(token)
+
     content = {
         "notifyUrl": "http://sheltered-plateau-48256.herokuapp.com/postTransactionstatus",
         "continueUrl": "https://sheltered-plateau-48256.herokuapp.com/waiterapp",
@@ -79,8 +79,6 @@ def getPaymentUri(request):
 
     jdata = json.loads(request.text);
     uri = jdata['redirectUri'];
-
-    print(uri)
 
     return HttpResponse(uri)
 
