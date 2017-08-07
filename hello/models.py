5b6651d4d1e7f79fd77eab47015a3f454fb76aaf
wojdziw <wojciech.dziwulski@gmail.com>
2017-08-07 13:43:29 +0200

    some changes

--- hello/models.py
+++ hello/models.py
@@ -3,7 +3,7 @@ from rest_framework.parsers import BaseParser
 
 class Transactionstatus(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
-    id = models.CharField(max_length=30, primary_key=True)
+    id = models.CharField(max_length=10, primary_key=True)
     status = models.CharField(max_length=30)
 
 class PlainTextParser(BaseParser):
