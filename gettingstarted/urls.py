07cc144e9f5ba629dd6c15797021968cd52f4ed5
wojdziw <wojciech.dziwulski@gmail.com>
2017-08-08 11:51:44 +0200

    Changed hello to waiterappbackend

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,18 +3,18 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+import waiterappbackend.views
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^waiterapp', hello.views.waiterapp, name='waiterapp'),
-    url(r'^postTransactionstatus', hello.views.postTransactionstatus, name='postTransactionstatus'),
-    url(r'^getTransactionstatus', hello.views.getTransactionstatus, name='getTransactionstatus'),
-    url(r'^getPaymentUri', hello.views.getPaymentUri, name='getPaymentUri'),
-    url(r'^getMenuPositions', hello.views.getMenuPositions, name='getMenuPositions'),
+    url(r'^$', waiterappbackend.views.index, name='index'),
+    url(r'^waiterapp', waiterappbackend.views.waiterapp, name='waiterapp'),
+    url(r'^postTransactionstatus', waiterappbackend.views.postTransactionstatus, name='postTransactionstatus'),
+    url(r'^getTransactionstatus', waiterappbackend.views.getTransactionstatus, name='getTransactionstatus'),
+    url(r'^getPaymentUri', waiterappbackend.views.getPaymentUri, name='getPaymentUri'),
+    url(r'^getMenuPositions', waiterappbackend.views.getMenuPositions, name='getMenuPositions'),
     url(r'^admin/', include(admin.site.urls)),
 ]
